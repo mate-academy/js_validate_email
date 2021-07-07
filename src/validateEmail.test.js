9a791153ffdb621d9f62fed77e83e90095c66a64
DidurOleg <diduroleg25@gmail.com>
2021-07-07 19:07:51 +0300

    Solution

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,11 +8,63 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof validateEmail('')).toBe('boolean');
   });
 
   it(`should return 'true' for the valid email...`, () => {
+    expect(validateEmail('qas@mail.com')).toBe(true);
+  });
 
+  it(`should return 'true' for the valid email...`, () => {
+    expect(validateEmail('q@m.c')).toBe(true);
+  });
+
+  it(`should return 'false' for the valid email...`, () => {
+    expect(validateEmail('*@*.*')).toBe(false);
+  });
+
+  it(`should return 'false' for the valid email...`, () => {
+    expect(validateEmail('вася@мыло.ком')).toBe(false);
+  });
+
+  it(`should return 'true' for the valid email...`, () => {
+    expect(validateEmail('123@mail.com')).toBe(true);
+  });
+
+  it(`should return 'true' for the valid email...`, () => {
+    expect(validateEmail('456@47.com')).toBe(true);
+  });
+
+  it(`should return 'false' for the email...`, () => {
+    expect(validateEmail('qas.mail.com')).toBe(false);
+  });
+
+  it(`should return 'false' for the email...`, () => {
+    expect(validateEmail('.qas@mail.com')).toBe(false);
+  });
+
+  it(`should return 'true' for the email...`, () => {
+    expect(validateEmail('QAS@MAIL.COM')).toBe(true);
+  });
+
+  it(`should return 'false' for the email...`, () => {
+    expect(validateEmail('qas@mail:com')).toBe(false);
+  });
+
+  it(`should return 'false' for the email...`, () => {
+    expect(validateEmail('qas@mail')).toBe(false);
+  });
+
+  it(`should return 'false' for the email...`, () => {
+    expect(validateEmail('!qas@mail.com')).toBe(false);
+  });
+
+  it(`should return 'false' for the email...`, () => {
+    expect(validateEmail('qas..das@mail.com')).toBe(false);
+  });
+
+  it(`should return 'false' for the email...`, () => {
+    expect(validateEmail('qas&A@mail.com')).toBe(false);
   });
 
   // write more tests here

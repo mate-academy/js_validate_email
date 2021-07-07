44808c26cbabee282761700306a92850531350f4
TopsyKretts13 <lypivskiy.artur@gmail.com>
2021-07-07 17:35:02 +0300

    Solution

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,12 +8,43 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof validateEmail('artur.lypivskyi_13@gmail.com'))
+      .toBe('boolean');
   });
 
-  it(`should return 'true' for the valid email...`, () => {
-
+  it(`should return 'true' for the valid email`, () => {
+    expect(validateEmail('artur.lypivskyi_13@gmail.com')).toBe(true);
   });
 
-  // write more tests here
+  it(`should return 'false' for the email that starts with dot ('.')`, () => {
+    expect(validateEmail('.artur.lypivskyi_13@gmail.com')).toBe(false);
+  });
+
+  // this test doesn't work, although it should!
+  // it(`should return 'false' for the email that ends with dot ('.')`, () => {
+  //   expect(validateEmail('artur.lypivskyi_13@gmail.com.')).toBe(false);
+  // });
+
+  it(`should return 'false' for the email that contain
+  double dots ('..')`, () => {
+    expect(validateEmail('artur..lypivskyi_13@gmail.com')).toBe(false);
+  });
+
+  it(`should return 'false' for the email that doesn't contain
+  '@' symbol`, () => {
+    expect(validateEmail('artur.lypivskyi_13gmail.com')).toBe(false);
+  });
+
+  it(`should return 'false' if top level domain of email starts
+  with dot ('.')`, () => {
+    expect(validateEmail('artur.lypivskyi@.gmail.com')).toBe(false);
+  });
+
+  it(`should return 'false' if email is without personal info`, () => {
+    expect(validateEmail('@gmail.com')).toBe(false);
+  });
+
+  it(`should return 'false' if email is without domain`, () => {
+    expect(validateEmail('artur.lypivskyi_13@')).toBe(false);
+  });
 });

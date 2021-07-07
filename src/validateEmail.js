d43b3366eb8d05d60553bf0c459cf67d75212eeb
Dana Oliynyk <dana.victorivna23@gmail.com>
2021-07-07 23:22:08 +0300

    email unit test

--- src/validateEmail.js
+++ src/validateEmail.js
@@ -7,7 +7,7 @@
  */
 function validateEmail(email) {
   // eslint-disable-next-line
-  const validEmailMask = new RegExp(/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\./i);
+  const validEmailMask = new RegExp(/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.[a-zA-Z]{2,4}$/);
 
   if (email.match(validEmailMask)) {
     return true;

35177ce1131dbb69ee4267d31ea6631686cee4c4
Iryna Khyzhynska <iryna.khyzhynska@gmail.com>
2021-07-07 18:33:13 +0300

    Solution

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,12 +8,42 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof validateEmail('SDF@scom')).toBe('boolean');
   });
 
-  it(`should return 'true' for the valid email...`, () => {
-
+  it(`should return 'true' for the valid email: sdf@asdflkj.com`, () => {
+    expect(validateEmail('sdf@asdflkj.com')).toBe(true);
   });
 
-  // write more tests here
+  it(`should return 'false' for the invalid email: ЖЖЖ@asdflkj.com`, () => {
+    expect(validateEmail('ЖЖЖ@asdflkj.com')).toBe(false);
+  });
+
+  it(`should return 'false' if top 
+  Level domein start with dot: opa@.com`, () => {
+    expect(validateEmail('opa@.com')).toBe(false);
+  });
+
+  it(`should return 'false' if email starts with dot: .ghj@asdflkj.com`, () => {
+    expect(validateEmail('.ghj@asdflkj.com')).toBe(false);
+  });
+
+  it(`should return 'false' if email 
+  contains double dots: gh:j@asdflkj.com`, () => {
+    expect(validateEmail('gh:j@asdflkj.com')).toBe(false);
+  });
+
+  it(`should return 'false' if email dont contain domain : opa@com`, () => {
+    expect(validateEmail('opa@com')).toBe(false);
+  });
+
+  it(`should return 'false' if email contains 
+  space in the beginning:  sdf@asdflkj.com`, () => {
+    expect(validateEmail(' sdf@asdflkj.com')).toBe(false);
+  });
+
+  it(`should return 'false' if email contains 
+  space in the middle: sd f@asdflkj.com`, () => {
+    expect(validateEmail('sd f@asdflkj.com')).toBe(false);
+  });
 });

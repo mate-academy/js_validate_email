80e6370b559a2dadc6f15395dce5d52ae25c0a80
Dmytro_Kuznietsov <kdvgym@gmail.com>
2021-07-07 16:50:37 +0300

    First check

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,12 +8,53 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof validateEmail('V.Zelen$k1y@ukr.net')).toBe('boolean');
   });
 
-  it(`should return 'true' for the valid email...`, () => {
-
+  it(`should return 'true' for the valid email`, () => {
+    expect(validateEmail('V.Zel3nsk1y@ukr.net')).toBe(true);
   });
 
-  // write more tests here
+  it(`should return 'false' for email without '@'`, () => {
+    expect(validateEmail('V.Zel3nsk1yukr.net')).toBe(false);
+  });
+
+  it(`should return 'false' for email without top-level domain`, () => {
+    expect(validateEmail('V.Zel3nsk1y@ukr')).toBe(false);
+  });
+
+  it(`should return 'false' for email without domain`, () => {
+    expect(validateEmail('V.Zel3nsk1y@.net')).toBe(false);
+  });
+
+  it(`should return 'false' for email without personal info part`, () => {
+    expect(validateEmail('@ukr.net')).toBe(false);
+  });
+
+  it(`should return 'false' if email field is empty`, () => {
+    expect(validateEmail('')).toBe(false);
+  });
+
+  it(`should return 'false' if entered only '@'`, () => {
+    expect(validateEmail('@')).toBe(false);
+  });
+
+  it(`should return 'false' if email starts on '.'`, () => {
+    expect(validateEmail('.V.Zel3nsk1y@.net')).toBe(false);
+  });
+
+  it(`should return 'false' if email have ':'`, () => {
+    expect(validateEmail('V:Zel3nsk1y@.net')).toBe(false);
+  });
+
+  it(`should return 'false' if email only with 
+  domain and top-level domain`, () => {
+    expect(validateEmail('gmail.com')).toBe(false);
+  });
+
+  it(`should return 'false' if email have spaces in domain`, () => {
+    expect(validateEmail('V.Zel3nsk1y@u k r.net')).toBe(false);
+  });
 });
+
+// не проходят тесты с этими сиволами ! # $ % & ' * + - / = ? ^ _ ` { | } ~

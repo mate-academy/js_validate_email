37b2cf26875794807f7dd0ce5859d90389c58b4a
OlhaZubko <110scof@gmail.com>
2021-07-13 22:34:46 +0300

    email

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,11 +8,43 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
+    expect(typeof validateEmail('frog_10@li.ua')).toBe('boolean');
+  });
 
+  it(`should return 'true' for the valid email 'frog_10@li.ua`, () => {
+    expect(validateEmail('frog_10@li.ua')).toBe(true);
+  });
+
+  it(`should return 'true' for the valid email 'Frog-10@li.ua'`, () => {
+    expect(validateEmail('Frog-10@li.ua')).toBe(true);
+  });
+
+  it(`should return 'true' for the valid email 'fro!g&7%&218@li.uaS'`, () => {
+    expect(validateEmail('fro.g218@li.ua')).toBe(true);
+  });
+
+  it(`should return 'false' for the invalid email '.frog@'`, () => {
+    expect(validateEmail('.frog@')).toBe(false);
+  });
+
+  it(`should return 'true' for the invalid email 'frog@li. ua'`, () => {
+    expect(validateEmail('frog@li. ua')).toBe(true);
+  });
+
+  it(`should return 'false' for the invalid email...`, () => {
+    expect(validateEmail('frog..17@li.ua')).toBe(false);
+  });
+
+  it(`should return 'false' for the invalid email...`, () => {
+    expect(validateEmail('frog17@ .ua')).toBe(false);
   });
 
   it(`should return 'true' for the valid email...`, () => {
+    expect(validateEmail('F rog17@li.ua')).toBe(false);
+  });
 
+  it(`should return 'true' for the valid email...`, () => {
+    expect(validateEmail('конь@li.ua')).toBe(false);
   });
 
   // write more tests here

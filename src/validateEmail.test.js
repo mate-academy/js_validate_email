98971b509848e4eea6fc2288b3c140bd8580f0e0
NikoLazur <lazur.niko@gmail.com>
2021-10-19 21:18:47 +0300

    Init solution

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,12 +8,41 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof validateEmail('example@mail.com')).toBe('boolean');
   });
 
-  it(`should return 'true' for the valid email...`, () => {
-
+  it(`should return 'true' for the valid email format`, () => {
+    expect(validateEmail('test12@mail.com')).toBeTruthy();
   });
 
-  // write more tests here
+  it(`should return 'false' for the email without "@" symbol`, () => {
+    expect(validateEmail('test_mail.com')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the email contains spaces`, () => {
+    expect(validateEmail('user mail.com')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the email starts with dot`, () => {
+    expect(validateEmail('.my@mail.com')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the email with 
+    domain part contains special characters`, () => {
+    expect(validateEmail('koala@m$il.com')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the email contains double dots`, () => {
+    expect(validateEmail('alias..2@mail.com')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the email contains 
+    dot before "@" symbol`, () => {
+    expect(validateEmail('mail1.@mail.com')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the email contains 
+    after "@" symbol`, () => {
+    expect(validateEmail('lama@.gmail.com')).toBeFalsy();
+  });
 });

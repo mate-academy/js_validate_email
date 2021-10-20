82253bc4129f9634c6cf87835feb60ac11f79988
mykolaPakhniuk <pakhniukm@gmail.com>
2021-10-20 14:44:37 +0300

    Solution

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,12 +8,35 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(validateEmail).toBeTruthy();
   });
 
-  it(`should return 'true' for the valid email...`, () => {
-
+  it(`should return 'true' for the valid email - pap1apa@papa.pap`, () => {
+    expect(validateEmail('pap1ap1a@papa.pap')).toBeTruthy();
   });
 
+  it(`should return 'true' for the valid email - t@q.c`, () => {
+    expect(validateEmail('t@q.c')).toBeTruthy();
+  });
+
+  it(`should return 'false' for the unvalid email - papapapapa.pap`, () => {
+    expect(validateEmail('papapapapa.pap')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the unvalid email - .papapa@papa.pap`, () => {
+    expect(validateEmail('.papapa@papa.pap')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the unvalid email - papapa@.pap`, () => {
+    expect(validateEmail('papapa@.pap')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the unvalid email - papapa@pap`, () => {
+    expect(validateEmail('papapa@pap')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the unvalid email - papapa@`, () => {
+    expect(validateEmail('papapa@')).toBeFalsy();
+  });
   // write more tests here
 });

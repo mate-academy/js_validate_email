b40f9f34ae196afab44211d58c7f5a6a2d2a1dcb
Bohdo <sooqiable@gmail.com>
2021-10-20 20:17:22 +0300

    Solution!

--- src/validateEmail.js
+++ src/validateEmail.js
@@ -5,6 +5,7 @@
  *
  * @returns {boolean}
  */
+
 function validateEmail(email) {
   // eslint-disable-next-line
   const validEmailMask = new RegExp(/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\./i);
--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,12 +8,51 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof validateEmail('')).toEqual('boolean');
   });
 
-  it(`should return 'true' for the valid email...`, () => {
-
+  it(`should return 'true' for the valid email`, () => {
+    expect(validateEmail('quality_21@gmail.com')).toEqual(true);
   });
 
-  // write more tests here
+  it(`should return 'false' for the invalid email
+    (does not contain required character '@')`, () => {
+    expect(validateEmail('quaility21_gmail.com')).toEqual(false);
+  });
+
+  it(`should return 'false' for the invalid email
+    (contain non-printable characters)`, () => {
+    expect(validateEmail('quality21 _ @gmail.com')).toEqual(false);
+  });
+
+  it(`should return 'false' for the invalid email
+    (only Latin letters)`, () => {
+    expect(validateEmail('qыlity21_gmail.com')).toEqual(false);
+  });
+
+  it(`should return 'false' for the invalid email
+    (special characters for
+    'personal_info' part only: '!$%&'*+-/=?^_{|}~')`, () => {
+    expect(validateEmail('qa:lity21@gmail.com')).toEqual(false);
+  });
+
+  it(`should return 'false' for the invalid email
+    ('.' cannot be the first symbol in the 'personal_info' part)`, () => {
+    expect(validateEmail('.quality21@gmail.com')).toEqual(false);
+  });
+
+  it(`should return 'false' for the invalid email
+    ('.' cannot be the last symbol in the 'personal_info' part)`, () => {
+    expect(validateEmail('quality21.@gmail.com')).toEqual(false);
+  });
+
+  it(`should return 'false' for the invalid email
+    ('.' cannot be the first symbol in the 'domain' part)`, () => {
+    expect(validateEmail('quality21@.gmail.com')).toEqual(false);
+  });
+
+  it(`should return 'false' for the invalid email
+    (cannot include '..')`, () => {
+    expect(validateEmail('q..ality21@gmail.com')).toEqual(false);
+  });
 });

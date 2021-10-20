e769d016718d256fc4e53996f4a918e3a8391688
Yana Baurina <baurina1@gmail.com>
2021-10-20 14:54:32 +0300

    Solution for validateEmail

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,12 +8,65 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof validateEmail('test@mail.com')).toBe('boolean');
   });
 
-  it(`should return 'true' for the valid email...`, () => {
-
+  it(`should return 'true' for the valid email 
+  with english letters in personal_info`, () => {
+    expect(validateEmail('test@mail.com')).toBeTruthy();
   });
 
-  // write more tests here
+  it(`should return 'true' for the valid email 
+  with english letters and capital letters in personal_info`, () => {
+    expect(validateEmail('tEst@mail.com')).toBeTruthy();
+  });
+
+  it(`should return 'true' for the valid email 
+  with english letters and digits in personal_info`, () => {
+    expect(validateEmail('test1@mail.com')).toBeTruthy();
+  });
+
+  it(`should return 'true' for the valid email 
+  with english letters and special characters in personal_info`, () => {
+    expect(validateEmail('test_r@mail.com')).toBeTruthy();
+  });
+
+  it(`should return 'true' for the valid email 
+  with one dot not on the first and last places in personal_info`, () => {
+    expect(validateEmail('test.test@mail.com')).toBeTruthy();
+  });
+
+  it(`should return 'true' for the valid email 
+  with '@' character between personal_info and top level domain`, () => {
+    expect(validateEmail('test.test@mail.com')).toBeTruthy();
+  });
+
+  it(`should return 'true' for the valid email 
+  with '.' character between top level domain and domain name`, () => {
+    expect(validateEmail('test.test@mail.com')).toBeTruthy();
+  });
+
+  it(`should return 'false' for the invalid email 
+  with the first '.' character of top level domain`, () => {
+    expect(validateEmail('test.test@.mail.com')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the invalid email 
+  with the first '.' character of personal_info`, () => {
+    expect(validateEmail('.test@mail.com')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the invalid email 
+  with double dots`, () => {
+    expect(validateEmail('te..st@mail.com')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the email without @`, () => {
+    expect(validateEmail('testmail.com')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the invalid email 
+  without domain name`, () => {
+    expect(validateEmail('test@mail')).toBeFalsy();
+  });
 });

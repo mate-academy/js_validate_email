5c47c58a3b633b60c597706c9591f26a52fe6ee7
Anton Zabolotny <toxober@gmail.com>
2021-10-20 16:46:30 +0300

    Solution

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,12 +8,47 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof validateEmail('')).toBe('boolean');
   });
 
-  it(`should return 'true' for the valid email...`, () => {
-
+  it(`should return 'true' for the valid email`, () => {
+    expect(validateEmail('mariya@mail.net')).toBeTruthy();
   });
 
-  // write more tests here
+  it(`should return 'false' for the email with
+   an extra dot in front of the domain name`, () => {
+    expect(validateEmail('mariya@.mail.net')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the email with symbol(s)
+   in the front of name`, () => {
+    expect(validateEmail('!mariya@mail.a.cademy')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the email with symbol(s) in the domain`, () => {
+    expect(validateEmail('mariya@ma!a.cademy')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the email without symbol @`, () => {
+    expect(validateEmail('mariyamail.com')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the email with symbol(s) after name`, () => {
+    expect(validateEmail('mariya~@mail.com')).toBeFalsy();
+  });
+
+  it(`should return 'true' for the email with symbol(s) 
+  in the middle of the name`, () => {
+    expect(validateEmail('mari_123@mail.com')).toBeTruthy();
+  });
+
+  it(`should return 'false' for a email with two dots 
+  in a row in the name`, () => {
+    expect(validateEmail('mari..ya@mail.com')).toBeFalsy();
+  });
+
+  it(`should return 'false' for a email with cyrillic 
+  symbols in the name`, () => {
+    expect(validateEmail('ффедор@mail.com')).toBeFalsy();
+  });
 });

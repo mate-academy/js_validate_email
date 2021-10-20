79d76119e2c53de21d14b1b23a6e3a85609d5fe2
zapchik <foramazon157@gmail.com>
2021-10-20 16:11:09 +0300

    Solution

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,11 +8,31 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof validateEmail('test@mail.com')).toBe('boolean');
   });
 
-  it(`should return 'true' for the valid email...`, () => {
+  it(`should return 'true' for the valid email(test_1@mail.com)`, () => {
+    expect(validateEmail('test_1@mail.com')).toBe(true);
+  });
 
+  it(`should return 'false' if the @ symbol is missing
+     (test_1mail.com)`, () => {
+    expect(validateEmail('test_1mail.com')).toBe(false);
+  });
+
+  it(`should return 'false' if the first part of the email begins with the "."
+     (.test_1@mail.com)`, () => {
+    expect(validateEmail('.test_1@mail.com')).toBe(false);
+  });
+
+  it(`should return 'false' if the domain name begins with a "."
+     (test_1@.mail.com)`, () => {
+    expect(validateEmail('test_1@.mail.com')).toBe(false);
+  });
+
+  it(`should return 'false' if the email contains Cyrillic letters
+     (тest_1@mail.com)`, () => {
+    expect(validateEmail('тest_1@mail.com')).toBe(false);
   });
 
   // write more tests here

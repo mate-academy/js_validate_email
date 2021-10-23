dbb73a7c1e769586236394411df02768d5fe3c38
Eugene Chornyi <winheisbox@gmail.com>
2021-10-23 17:25:46 +0300

    HW

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,12 +8,52 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof validateEmail('')).toBe('boolean');
   });
 
   it(`should return 'true' for the valid email...`, () => {
-    expect(validateEmail('adolas+666@gmail.com.')).toBe(true);
+    expect(validateEmail('black888@gmail.com.')).toBe(true);
   });
 
   // write more tests here
+
+  it(`should return 'false' for email without '@'`, () => {
+    expect(validateEmail('black888gmail.com')).toBe(false);
+  });
+
+  it(`should return 'false' for email with double '@'`, () => {
+    expect(validateEmail('black@888@gmail.com')).toBe(false);
+  });
+
+  it(`should return 'false' if '.' is the first char.`, () => {
+    expect(validateEmail('.black888@gmail.com')).toBe(false);
+  });
+
+  it(`should return 'false' if domain starts with '.'`, () => {
+    expect(validateEmail('black888@gmailcom')).toBe(false);
+  });
+
+  it(`should return 'false' if '.' is before '.'`, () => {
+    expect(validateEmail('black..888@gmail.com')).toBe(false);
+  });
+
+  it(`should return 'false' if email is without username`, () => {
+    expect(validateEmail('@gmail.com')).toBe(false);
+  });
+
+  it(`should return 'false' if email is without domain`, () => {
+    expect(validateEmail('black888@')).toBe(false);
+  });
+
+  it(`should return 'false' if email is without domain first part`, () => {
+    expect(validateEmail('black888@.com')).toBe(false);
+  });
+
+  it(`should return 'false' if email is without domain second part`, () => {
+    expect(validateEmail('black888@gmail')).toBe(false);
+  });
+
+  it(`should return 'false' if email have spaces`, () => {
+    expect(validateEmail('black888@ gmail.com')).toBe(false);
+  });
 });

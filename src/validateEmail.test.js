7b6ec462648b616b0e84ea0ddd8ec3bc02aff2a9
Dmytro Semenov <Dmytro.Semenov@tx.local>
2022-10-11 18:45:40 +0300

    Solution

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,12 +8,40 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof validateEmail('adolas+666@gmail.com.')).toBe('boolean');
   });
 
-  it(`should return 'true' for the valid email...`, () => {
-    expect(validateEmail('adolas+666@gmail.com.')).toBe(true);
+  it(`should return 'true' for the valid email`, () => {
+    expect(validateEmail('test@mail.com')).toBeTruthy();
   });
 
-  // write more tests here
+  it(`should return 'false' if email includes a letter`
+   + ` from another alphabet`, () => {
+    expect(validateEmail('teФst@mail.com')).toBeFalsy();
+  });
+
+  it(`should return 'false' if email without '@' or '.'`, () => {
+    expect(validateEmail('testmail.com')).toBeFalsy();
+    expect(validateEmail('test@mailcom')).toBeFalsy();
+  });
+
+  it(`should return 'false' if email includes not allowed characters`, () => {
+    expect(validateEmail('t!est@mail.com')).toBeFalsy();
+    expect(validateEmail('t$est@mail.com')).toBeFalsy();
+    expect(validateEmail('test@mai*l.com')).toBeFalsy();
+    expect(validateEmail('test@mai:l.com')).toBeFalsy();
+  });
+
+  it(`should return 'false' if email begins with '.'`, () => {
+    expect(validateEmail('.test@mail.com')).toBeFalsy();
+  });
+
+  it(`should return 'false' if email includes double dots`
+    + ` in personal_info part`, () => {
+    expect(validateEmail('te:st@mail.com')).toBeFalsy();
+  });
+
+  it(`should return 'false' if email is empty`, () => {
+    expect(validateEmail('')).toBeFalsy();
+  });
 });

ddc5ad99387d8d92fdbaf5c0e9ae36c0978c99d4
Ihor Duhanets <igor_duganets@ukr.net>
2023-06-11 12:21:59 +0300

    Solution

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,13 +8,57 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof validateEmail('test@mail.com')).toBe('boolean');
   });
 
   it(`should return 'true' for the valid email`, () => {
-    expect(validateEmail('test838@gmail.com.'))
+    expect(validateEmail('Alex.test-838@gmail.com'))
       .toBeTruthy();
   });
 
-  // write more tests here
+  it(`should return 'false' when the email not contains @`, () => {
+    expect(validateEmail('test838gmail.com'))
+      .toBe(false);
+  });
+
+  it(`should return 'false' for the email that begins with dot`, () => {
+    expect(validateEmail('.alextest-838@gmail.com'))
+      .toBe(false);
+  });
+
+  it(`should return 'false' for the email when personal 
+  info part ends with dot`, () => {
+    expect(validateEmail('alextest-838.@gmail.com'))
+      .toBe(false);
+  });
+
+  it(`should return 'false' for the email when personal 
+  info part has two dots one after another`, () => {
+    expect(validateEmail('alextest..838@gmail.com'))
+      .toBe(false);
+  });
+
+  it(`should return 'false' for the email when personal 
+  info part contains double dots`, () => {
+    expect(validateEmail('alextest"838@gmail.com'))
+      .toBe(false);
+  });
+
+  it(`should return 'false' for the email that contains 
+   not allowed special character`, () => {
+    expect(validateEmail('alextest/838@gmail.com'))
+      .toBe(false);
+  });
+
+  it(`should return 'false' for the email when 
+   domain part begins with dot`, () => {
+    expect(validateEmail('alextest-838@.gmail.com'))
+      .toBe(false);
+  });
+
+  it(`should return 'true' domain name part 
+   contains digits and hyphens`, () => {
+    expect(validateEmail('Alex.test-838@gmail-7.com'))
+      .toBeTruthy();
+  });
 });

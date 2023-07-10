cd9daf00f4ed066fa5771ffe956e0dcd34c6c08f
Dmytro Budynskyi <dbudinskyy@gmail.com>
2023-07-10 08:59:12 +0200

    Solution

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -17,4 +17,38 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   // write more tests here
+  it(`should return 'false' for the email with the characters
+    not from ASCII`, () => {
+    expect(validateEmail('Ыpersonal_info@gmail.com')).toBe(false);
+  });
+
+  it(`should return 'true' for the email with the digits`, () => {
+    expect(validateEmail('personal_info123@gmail.com')).toBe(true);
+  });
+
+  it(`should return 'true' for the email with "_"`, () => {
+    expect(validateEmail('personal_info@gmail.com')).toBe(true);
+  });
+
+  it(`should return 'false' for the email starts with "." `, () => {
+    expect(validateEmail('.personal_info@gmail.com')).toBe(false);
+  });
+
+  it(`should return 'false' for the email without @`, () => {
+    expect(validateEmail('personal_infogmail.com')).toBe(false);
+  });
+
+  it(`should return 'false' for the email where domain starts with "."`, () => {
+    expect(validateEmail('personal_infog@.mail.com')).toBe(false);
+  });
+
+  it(`should return 'false' for the email where
+    domain don't includes "."`, () => {
+    expect(validateEmail('personal_info@gmailcom')).toBe(false);
+  });
+
+  it(`should return 'true' for the email where
+    domain don't includes digits`, () => {
+    expect(validateEmail('personal_info@gmail1.com')).toBe(true);
+  });
 });

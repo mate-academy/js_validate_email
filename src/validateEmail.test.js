28ae1f4c9a712211b9affff8bbf6aed3ac0795a6
Joanna Konopacka <joanna.konopacka@hotmail.com>
2023-11-01 22:35:47 +0100

    solution

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -1,20 +1,19 @@
-'use strict';
+"use strict";
 
-describe(`Function 'validateEmail':`, () => {
-  const validateEmail = require('./validateEmail');
+const validateEmail = require("./validateEmail");
 
-  it(`should be declared`, () => {
-    expect(validateEmail).toBeInstanceOf(Function);
+describe("validateEmail", () => {
+  it("should return true for a valid email", () => {
+    expect(validateEmail("test@mail.com")).toBe(true);
+    expect(validateEmail("t@q.c")).toBe(true);
   });
 
-  it(`should return boolean`, () => {
-
+  it("should return false for an invalid email", () => {
+    expect(validateEmail("false@email")).toBe(false);
+    expect(validateEmail("invalid.email@")).toBe(false);
+    expect(validateEmail(".start.with.dot@mail.com")).toBe(false);
+    expect(validateEmail("double..dots@mail.com")).toBe(false);
+    expect(validateEmail("invalid$character@mail.com")).toBe(false);
+    expect(validateEmail("test@!invalidcharacter.com")).toBe(false);
   });
-
-  it(`should return 'true' for the valid email`, () => {
-    expect(validateEmail('test838@gmail.com.'))
-      .toBeTruthy();
-  });
-
-  // write more tests here
 });

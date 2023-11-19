1d6b56ca56ce35b70eeb8c89edad0525bf837081
Illia Vinkov <illia.vinkov@gmail.com>
2023-11-19 10:59:45 +0100

    Solution

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,13 +8,75 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
+    const result = validateEmail('test@mail.com');
 
+    expect(typeof result).toBe('boolean');
   });
 
   it(`should return 'true' for the valid email`, () => {
-    expect(validateEmail('test838@gmail.com.'))
-      .toBeTruthy();
+    const result = validateEmail('test838@gmail.com.');
+
+    expect(result).toBe(true);
   });
 
-  // write more tests here
+  it(`should return 'false' if email does not include @`, () => {
+    const result = validateEmail('testmail.com');
+
+    expect(result).toBe(false);
+  });
+
+  it(`should return 'false' if email includes `
+    + `not English letters (Aa-Zz)`, () => {
+    const result = validateEmail('test@яmail.com');
+
+    expect(result).toBe(false);
+  });
+
+  it(`should return 'false' if email starts with .`, () => {
+    const result = validateEmail('.test@mail.com');
+
+    expect(result).toBe(false);
+  });
+
+  it(`should return 'false' if email ends with .`, () => {
+    const result = validateEmail('.test@mail.com.');
+
+    expect(result).toBe(false);
+  });
+
+  it(`should return 'false' if domain starts with .`, () => {
+    const result = validateEmail('test@.mail.com');
+
+    expect(result).toBe(false);
+  });
+
+  it(`should return 'false' if email has special symbol`, () => {
+    const result = validateEmail('tes#t@mail.com');
+
+    expect(result).toBe(false);
+  });
+
+  it(`should return 'false' if email has . come one after the other`, () => {
+    const result = validateEmail('t..est@mail.com');
+
+    expect(result).toBe(false);
+  });
+
+  it(`should return 'true' if email includes digits`, () => {
+    const result = validateEmail('tes1t@mail.com');
+
+    expect(result).toBe(true);
+  });
+
+  it(`should return 'true' if email includes - or _`, () => {
+    const result = validateEmail('te_s-t@mail.com');
+
+    expect(result).toBe(true);
+  });
+
+  it(`should return 'true' if email includes .`, () => {
+    const result = validateEmail('t.est@mail.com');
+
+    expect(result).toBe(true);
+  });
 });

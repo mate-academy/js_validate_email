a3b56a0af8739fc0c907b420af5ec149606885dd
VladSlob <141657806+VladSlob@users.noreply.github.com>
2023-11-22 08:05:18 +0200

    validateEmail.test.js

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -16,5 +16,37 @@ describe(`Function 'validateEmail':`, () => {
       .toBeTruthy();
   });
 
-  // write more tests here
-});
+    it(`should return 'false' for the invalid email`, () => {
+    expect(validateEmail('test838@gmail..com')).toBeFalsy();
+    expect(validateEmail('test838@.com')).toBeFalsy();
+    expect(validateEmail('.test838@gmail.com')).toBeFalsy();
+    expect(validateEmail('test838@gmail.com.')).toBeFalsy();
+    expect(validateEmail('test838@gmail com')).toBeFalsy();
+    expect(validateEmail('test838@gmail,com')).toBeFalsy();
+    expect(validateEmail('test838@gmail@com')).toBeFalsy();
+    expect(validateEmail('test838@gmail!com')).toBeFalsy();
+  });
+
+  it(`should return 'true' for the valid email`, () => {
+    expect(validateEmail('test838@gmail.com')).toBeTruthy();
+    expect(validateEmail('t@q.c')).toBeTruthy();
+    expect(validateEmail('test.name_838@gmail.com')).toBeTruthy();
+    expect(validateEmail('test-name_838@gmail.com')).toBeTruthy();
+    expect(validateEmail('test.name_838@subdomain.domain.com')).toBeTruthy();
+  });
+
+  it(`should return 'false' for the email without '@'`, () => {
+    expect(validateEmail('test838gmail.com')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the email starting with '.'`, () => {
+    expect(validateEmail('.test838@gmail.com')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the email ending with '.'`, () => {
+    expect(validateEmail('test838@gmail.com.')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the email with '..'`, () => {
+    expect(validateEmail('test838..test@gmail.com')).toBeFalsy();
+  });

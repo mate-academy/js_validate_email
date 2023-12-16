7f9282588932dd34946efaa8f412d396b2dd78c0
Eugene Khablenko <eugene.khablenko@gmail.com>
2023-12-16 05:08:08 +0200

    solution

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,13 +8,66 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
+    const result = validateEmail('test@email.com');
 
+    expect(typeof result).toBe('boolean');
   });
 
   it(`should return 'true' for the valid email`, () => {
-    expect(validateEmail('test838@gmail.com.'))
-      .toBeTruthy();
+    expect(validateEmail('test838@gmail.com.')).toBeTruthy();
   });
 
-  // write more tests here
+  it(`
+    should return 'false' for the email
+    without '@' character
+  `, () => {
+    const result = validateEmail('testgmail.com');
+
+    expect(result).toBeFalsy();
+  });
+
+  it(`
+    should return 'false' for the email
+    with non-latin character
+  `, () => {
+    const result = validateEmail('testы@gmail.com');
+
+    expect(result).toBeFalsy();
+  });
+
+  it(`
+    should return 'false' for the email
+    that contains a non-latin character
+  `, () => {
+    const result = validateEmail('testы@gmail.com');
+
+    expect(result).toBeFalsy();
+  });
+
+  it(`
+    should return 'false' for the email
+    that contains a '$' character
+  `, () => {
+    const result = validateEmail('test$@gmail.com');
+
+    expect(result).toBeFalsy();
+  });
+
+  it(`
+    should return 'false' for the email
+    that contains a '..' in the name part
+  `, () => {
+    const result = validateEmail('test..me@gmail.com');
+
+    expect(result).toBeFalsy();
+  });
+
+  it(`
+    should return 'false' for the email
+    that starts with a '.' in the name part
+  `, () => {
+    const result = validateEmail('.test.me@gmail.com');
+
+    expect(result).toBeFalsy();
+  });
 });

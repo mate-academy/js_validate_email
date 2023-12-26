dbfe2c2d67b46650062b092b7b6f830ba399bf2d
Kate <ekaterina2002aleks@gmail.com>
2023-12-26 10:38:09 +0200

    Solution

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,7 +8,8 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof validateEmail('test838@gmail.com.'))
+      .toEqual('boolean');
   });
 
   it(`should return 'true' for the valid email`, () => {
@@ -16,5 +17,18 @@ describe(`Function 'validateEmail':`, () => {
       .toBeTruthy();
   });
 
-  // write more tests here
+  it(`should return 'false' for the invalid email with dot`, () => {
+    expect(validateEmail('.test838@gmail.com.'))
+      .toBeFalsy();
+  });
+
+  it(`2should return 'false' for the invalid email with double dot`, () => {
+    expect(validateEmail('test..838@gmail.com.'))
+      .toBeFalsy();
+  });
+
+  it(`should return 'false' for the invalid email with special symbols`, () => {
+    expect(validateEmail('test8!%38@gmail.com.'))
+      .toBeFalsy();
+  });
 });

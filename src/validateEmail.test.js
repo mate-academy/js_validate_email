a99cf2ff08f6a8ac0f7d3fbdf2b655d0213790e5
bla2001 <blasmartlab@gmail.com>
2024-03-24 23:26:10 +0200

    Solution

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,13 +8,67 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
+    const result = validateEmail('test@test.mate');
 
+    expect(typeof result).toBe('boolean');
   });
 
-  it(`should return 'true' for the valid email`, () => {
-    expect(validateEmail('test838@gmail.com.'))
-      .toBeTruthy();
+  describe(`should return 'true' for `, () => {
+    it(`email that contain personal and domain`
+        + ` parts separated by @`, () => {
+      const result = validateEmail('test1@test.mate');
+
+      expect(result).toBe(true);
+    });
+
+    it(`email that contain '_' symbol in the personal part`, () => {
+      const result = validateEmail('_test2@test.mate');
+
+      expect(result).toBe(true);
+    });
+
+    it(`email that contain '_' symbol in the domain part`, () => {
+      const result = validateEmail('test2@test_test.mate');
+
+      expect(result).toBe(true);
+    });
+
+    it(`email that contain allowed '.' symbol in personal part`, () => {
+      const result = validateEmail('test.4@test.mate');
+
+      expect(result).toBe(true);
+    });
+
+    it(`email that contain '-' symbol in the domain part`, () => {
+      const result = validateEmail('other.email@with-hyphen-example.com');
+
+      expect(result).toBe(true);
+    });
+
+    it(`email that contain '-' symbol in the personal part`, () => {
+      const result = validateEmail('email-with-hyphen@example.com');
+
+      expect(result).toBe(true);
+    });
   });
 
-  // write more tests here
+  describe(`should return 'false' for `, () => {
+    it(`email with missed @ symbol`, () => {
+      const result = validateEmail('invalid.example.com');
+
+      expect(result).toBe(false);
+    });
+
+    it('email with double dot in personal_info', () => {
+      const result = validateEmail('invalid..email@example.com');
+
+      expect(result).toBe(false);
+    });
+
+    it('email that starts with dot in personal_info', () => {
+      const result = validateEmail('.invalid@example.com');
+
+      expect(result).toBe(false);
+    });
+  });
 });

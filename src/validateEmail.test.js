558897a78759fdf86e1b1ab6d62523c0e8541065
TykhonKozachenko <tooliewae@gmail.com>
2024-04-14 16:20:18 +0200

    tests

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -4,17 +4,56 @@ describe(`Function 'validateEmail':`, () => {
   const validateEmail = require('./validateEmail');
 
   it(`should be declared`, () => {
-    expect(validateEmail).toBeInstanceOf(Function);
+    expect(validateEmail)
+      .toBeInstanceOf(Function);
   });
 
   it(`should return boolean`, () => {
+    const result = validateEmail('testemail1@gmail.com');
 
+    expect(typeof result)
+      .toBe('boolean');
   });
 
   it(`should return 'true' for the valid email`, () => {
-    expect(validateEmail('test838@gmail.com.'))
+    const validEmail = validateEmail('TeSt1-_.y@gmail.com');
+
+    expect(validEmail)
       .toBeTruthy();
   });
 
-  // write more tests here
+  it(`should return 'false' for the email without @`, () => {
+    const invalidEmail = validateEmail('test838gmail.com');
+
+    expect(invalidEmail)
+      .toBeFalsy();
+  });
+
+  it(`should return 'false' for the email with . as first character`, () => {
+    const invalidEmail = validateEmail('.tes-t@gmail.com');
+
+    expect(invalidEmail)
+      .toBeFalsy();
+  });
+
+  it(`should return 'false' for the email with . as last character`, () => {
+    const invalidEmail = validateEmail('tes-1t.@gmail.com.');
+
+    expect(invalidEmail)
+      .toBeFalsy();
+  });
+
+  it(`should return 'false' for the email with double dots`, () => {
+    const invalidEmail = validateEmail('tes1:-t@gmail.com.');
+
+    expect(invalidEmail)
+      .toBeFalsy();
+  });
+
+  it(`should return 'false' for the email with not allowed characters`, () => {
+    const invalidEmail = validateEmail('tes1!$%+-t@gmail.com.');
+
+    expect(invalidEmail)
+      .toBeFalsy();
+  });
 });

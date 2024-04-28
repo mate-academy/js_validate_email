7e828946cc63863d2bfcd41f7e85f000811e9ffa
Rozdorozhnii <rozdorozhnii@ukr.net>
2024-04-28 22:13:57 +0300

    Solution_v1.0

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,7 +8,7 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof validateEmail('')).toBe('boolean');
   });
 
   it(`should return 'true' for the valid email`, () => {
@@ -16,5 +16,87 @@ describe(`Function 'validateEmail':`, () => {
       .toBeTruthy();
   });
 
-  // write more tests here
+  it(`should return 'true' for email with digits only in username`, () => {
+    expect(validateEmail('838@gmail.com.'))
+      .toBeTruthy();
+  });
+
+  it(`should return 'true' for email with underscore '_' in username`, () => {
+    expect(validateEmail('_@gmail.com.'))
+      .toBeTruthy();
+  });
+
+  it(`should return 'true' for email with hyphen '-' in username`, () => {
+    expect(validateEmail('-@gmail.com.'))
+      .toBeTruthy();
+  });
+
+  it(`should return 'false' for email starts with dot '.' in username`, () => {
+    expect(validateEmail('.test838@mail.com'))
+      .toBeFalsy();
+  });
+
+  it(`should return 'false' for email ends with dot '.' in username`, () => {
+    expect(validateEmail('test838.@mail.com'))
+      .toBeFalsy();
+  });
+
+  it(`should return 'false' for email includes double dot '..' in username`,
+    () => {
+      expect(validateEmail('test..838@mail.com'))
+        .toBeFalsy();
+    });
+
+  it(`should return 'false' for email includes`
+    + ` exclamation mark '!' in username`, () => {
+    expect(validateEmail('test!838@mail.com'))
+      .toBeFalsy();
+  });
+
+  it(`should return 'false' for email includes`
+    + ` question mark '?' in username`, () => {
+    expect(validateEmail('test?838@mail.com'))
+      .toBeFalsy();
+  });
+
+  it(`should return 'false' for email includes`
+    + ` quote "'" in username`, () => {
+    expect(validateEmail(`test'838@mail.com`))
+      .toBeFalsy();
+  });
+
+  it(`should return 'false' for email includes`
+    + ` arithmetic operations mark in username`, () => {
+    expect(validateEmail('test+838@mail.com'))
+      .toBeFalsy();
+  });
+
+  it(`should return 'false' for email includes`
+    + ` currency symbol in username`, () => {
+    expect(validateEmail('test$838@mail.com'))
+      .toBeFalsy();
+  });
+
+  it(`should return 'false' for email includes`
+    + ` ampersand '&' in username`, () => {
+    expect(validateEmail('test&838@mail.com'))
+      .toBeFalsy();
+  });
+
+  it(`should return 'false' for email includes`
+    + ` curly brace '{' in username`, () => {
+    expect(validateEmail('test&838@mail.com'))
+      .toBeFalsy();
+  });
+
+  it(`should return 'false' for email with non-English letter in username`,
+    () => {
+      expect(validateEmail('tßäö838@mail.com'))
+        .toBeFalsy();
+    });
+
+  it(`should return 'false' for email starts with dot '.' in domain`, () => {
+    expect(validateEmail('test838@.mail.com'))
+      .toBeFalsy();
+  });
 });

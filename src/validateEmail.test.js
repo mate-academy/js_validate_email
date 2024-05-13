ed78509be632840a8fad8fef229812d30ef18065
Yehor-f2006 <151622806+Yehor-f2006@users.noreply.github.com>
2024-05-13 13:12:28 +0300

    solution

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,13 +8,43 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
+    const result = validateEmail('');
 
+    expect(typeof result).toBe('boolean');
   });
 
   it(`should return 'true' for the valid email`, () => {
-    expect(validateEmail('test838@gmail.com.'))
+    expect(validateEmail('test838@gmail.com'))
       .toBeTruthy();
   });
 
-  // write more tests here
+  it('accepts only Latin characters', () => {
+    const result = validateEmail('testЩ838@gmail.com');
+
+    expect(result).toBeFalsy();
+  });
+
+  it(`dots cannot go before the '@' sign`, () => {
+    const result = validateEmail('test.@gmail.com');
+
+    expect(result).toBeFalsy();
+  });
+
+  it('no consecutive dots', () => {
+    const result = validateEmail('te...st@gmail.com');
+
+    expect(result).toBeFalsy();
+  });
+
+  it('cannot start with a dot', () => {
+    const result = validateEmail('.test@gmail.com');
+
+    expect(result).toBeFalsy();
+  });
+
+  it('a top-level domain must be present', () => {
+    const result = validateEmail('test@gmail');
+
+    expect(result).toBeFalsy();
+  });
 });

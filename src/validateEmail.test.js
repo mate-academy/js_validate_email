e0e8c6453b5a7e2ebca25705b9b9f1fda49d78e9
Roksolana Kalytyn <roksa.kalytyn@gmail.com>
2024-05-19 00:31:47 +0300

    Solution

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,7 +8,7 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof validateEmail('test12@mail.com')).toBe('boolean');
   });
 
   it(`should return 'true' for the valid email`, () => {
@@ -16,5 +16,49 @@ describe(`Function 'validateEmail':`, () => {
       .toBeTruthy();
   });
 
-  // write more tests here
+  it(`should return 'true' for email with '-' symbol`, () => {
+    expect(validateEmail('test-er@mail.com'))
+      .toBeTruthy();
+  });
+
+  it(`should return 'true' for email with '_' symbol`, () => {
+    expect(validateEmail('test_er@mail.com'))
+      .toBeTruthy();
+  });
+
+  it(`should return 'true' for email`
+  + `with letters, digits, hypens and dots`, () => {
+    expect(validateEmail('tes.t_er123@mail.com'))
+      .toBeTruthy();
+  });
+
+  it(`should return 'false' for email with special characters`, () => {
+    expect(validateEmail('test!er@mail.com'))
+      .toBe(false);
+  });
+
+  it(`should return 'false' for email without dot`, () => {
+    expect(validateEmail('tester@mailcom'))
+      .toBe(false);
+  });
+
+  it(`should return 'false' for email that starts with dot`, () => {
+    expect(validateEmail('.tester@mail.com'))
+      .toBe(false);
+  });
+
+  it(`should return 'false' for email with domain that starts with dot`, () => {
+    expect(validateEmail('tester@.mail.com'))
+      .toBe(false);
+  });
+
+  it(`should return 'false' for email with double dots`, () => {
+    expect(validateEmail('tes..ter@mail.com'))
+      .toBe(false);
+  });
+
+  it(`should return 'false' for email without '@'`, () => {
+    expect(validateEmail('testermail.com'))
+      .toBe(false);
+  });
 });

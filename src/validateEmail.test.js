bb447d62094cef384a0a168c08da8fbe767ddecb
Andrii Rykal <andreuka79@gmail.com>
2024-09-12 14:34:38 +0300

    Solution

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,7 +8,9 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
+    const boolean = validateEmail('');
 
+    expect(typeof boolean).toBe('boolean');
   });
 
   it(`should return 'true' for the valid email`, () => {
@@ -16,5 +18,51 @@ describe(`Function 'validateEmail':`, () => {
       .toBeTruthy();
   });
 
-  // write more tests here
+  it(`should return 'false' for the email that`
+    + ` contains non-English letters`, () => {
+    expect(validateEmail('тест838@gmail.com.'))
+      .toBeFalsy();
+  });
+
+  it(`should return 'false' for the email that`
+    + ` starts with a dot`, () => {
+    expect(validateEmail('.test838@gmail.com.'))
+      .toBeFalsy();
+  });
+
+  it(`should return false if personal information`
+    + ` ends with a dot`, () => {
+    expect(validateEmail('test838.@gmail.com.'))
+      .toBeFalsy();
+  });
+
+  it(`should return 'false' for the email that`
+    + ` contains multiple periods in a row`, () => {
+    expect(validateEmail('test..838@gmail.com.'))
+      .toBeFalsy();
+  });
+
+  it(`should return false if the domain`
+    + ` start to dot`, () => {
+    expect(validateEmail('test838@.gmail.com.'))
+      .toBeFalsy();
+  });
+
+  it(`should return 'false' if the email `
+    + ` contains ':'`, () => {
+    expect(validateEmail('test:838@gmail.com.'))
+      .toBeFalsy();
+  });
+
+  it(`should return 'false' if the email`
+    + ` does not contain '@'`, () => {
+    expect(validateEmail('test838gmail.com.'))
+      .toBeFalsy();
+  });
+
+  it(`should return 'false' if the email contains`
+    + ` not allowed characters: ! $ % & ' * + / = ? ^ { | } ~`, () => {
+    expect(validateEmail('test$838@gmail.com.'))
+      .toBeFalsy();
+  });
 });

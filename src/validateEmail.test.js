b4cde6cfdab936d0ceaeb046d36698b8d9cc5635
Pavlo Petrashevskyi <dragonthunderaddsdp@gmail.com>
2024-11-03 15:45:05 +0200

    tests for validateEmail were created

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,13 +8,30 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof validateEmail('goodEmail@.com')).toBe('boolean');
   });
 
   it(`should return 'true' for the valid email`, () => {
-    expect(validateEmail('test838@gmail.com.'))
-      .toBeTruthy();
+    expect(validateEmail('test838@gmail.com.')).toBeTruthy();
+    expect(validateEmail('userName.userSurname@gmail.com.')).toBeTruthy();
+    expect(validateEmail('anabella123Syphora@g.com.')).toBeTruthy();
+    expect(validateEmail('SirkoIvan@uk.com')).toBeTruthy();
   });
 
-  // write more tests here
+  it(`should return 'true' for the valid email`, () => {
+    expect(validateEmail('test!@#!@#!@gmail.com.')).toBeFalsy();
+    expect(validateEmail('.userName.userSurname.@gmail.com.')).toBeFalsy();
+    expect(validateEmail('anabella123Syphorag.com.')).toBeFalsy();
+    expect(validateEmail('hjkhkjh5675.v')).toBeFalsy();
+  });
+
+  it(`should throw an error if email is not a string`, () => {
+    expect(() => validateEmail()).toThrow();
+    expect(() => validateEmail({})).toThrow();
+    expect(() => validateEmail([])).toThrow();
+    expect(() => validateEmail(null)).toThrow();
+    expect(() => validateEmail(NaN)).toThrow();
+    expect(() => validateEmail(false)).toThrow();
+    expect(() => validateEmail(4)).toThrow();
+  });
 });

4d76bd1a3d23412f474d31712c0c7083a2a2b9f6
Jakub <paniczochy@interia.pl>
2024-11-14 21:48:06 +0100

    solution

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -70,70 +70,39 @@ describe(`Function 'validateEmail':`, () => {
       .toBe(false);
   });
 
-  it(`should return false if "! $ % & ' * + / = ? ^ { | } ~" is in email`
-    , () => {
-      const mail1 = 'email!@test.com';
-      const mail2 = 'email$@test.com';
-      const mail3 = 'email%@test.com';
-      const mail4 = 'email&@test.com';
-      const mail5 = "email'@test.com";
-      const mail6 = 'email*@test.com';
-      const mail7 = 'email+@test.com';
-      const mail8 = 'email/@test.com';
-      const mail9 = 'email=@test.com';
-      const mail10 = 'email?@test.com';
-      const mail11 = 'email^@test.com';
-      const mail12 = 'email{@test.com';
-      const mail13 = 'email|@test.com';
-      const mail14 = 'email}@test.com';
-      const mail15 = 'email~@test.com';
-
-      expect(validateEmail(mail1))
-        .toBe(false);
-
-      expect(validateEmail(mail2))
-        .toBe(false);
-
-      expect(validateEmail(mail3))
-        .toBe(false);
-
-      expect(validateEmail(mail4))
-        .toBe(false);
-
-      expect(validateEmail(mail5))
-        .toBe(false);
-
-      expect(validateEmail(mail6))
-        .toBe(false);
-
-      expect(validateEmail(mail7))
-        .toBe(false);
-
-      expect(validateEmail(mail8))
-        .toBe(false);
-
-      expect(validateEmail(mail9))
-        .toBe(false);
-
-      expect(validateEmail(mail10))
-        .toBe(false);
-
-      expect(validateEmail(mail11))
-        .toBe(false);
-
-      expect(validateEmail(mail12))
-        .toBe(false);
-
-      expect(validateEmail(mail13))
-        .toBe(false);
-
-      expect(validateEmail(mail14))
-        .toBe(false);
-
-      expect(validateEmail(mail15))
-        .toBe(false);
-    });
-
+  it(`should return "false" if invalid characters "! $ % & ' *
+    + / = ? ^ { | } ~" are in email`, () => {
+   const mail1 = 'email!@test.com';
+   const mail2 = 'email$@test.com';
+   const mail3 = 'email%@test.com';
+   const mail4 = 'email&@test.com';
+   const mail5 = "email'@test.com";
+   const mail6 = 'email*@test.com';
+   const mail7 = 'email+@test.com';
+   const mail8 = 'email/@test.com';
+   const mail9 = 'email=@test.com';
+   const mail10 = 'email?@test.com';
+   const mail11 = 'email^@test.com';
+   const mail12 = 'email{@test.com';
+   const mail13 = 'email|@test.com';
+   const mail14 = 'email}@test.com';
+   const mail15 = 'email~@test.com';
+   expect(validateEmail(mail1)).toBe(false);
+   expect(validateEmail(mail2)).toBe(false);
+   expect(validateEmail(mail3)).toBe(false);
+   expect(validateEmail(mail4)).toBe(false);
+   expect(validateEmail(mail5)).toBe(false);
+   expect(validateEmail(mail6)).toBe(false);
+   expect(validateEmail(mail7)).toBe(false);
+   expect(validateEmail(mail8)).toBe(false);
+   expect(validateEmail(mail9)).toBe(false);
+   expect(validateEmail(mail10)).toBe(false);
+   expect(validateEmail(mail11)).toBe(false);
+   expect(validateEmail(mail12)).toBe(false);
+   expect(validateEmail(mail13)).toBe(false);
+   expect(validateEmail(mail14)).toBe(false);
+   expect(validateEmail(mail15)).toBe(false);
+ });
   it('should return  "false" for email without personal-info', () => {
     expect(validateEmail('@Test.com'))
       .toBe(false);

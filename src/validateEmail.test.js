905ef99b56072468e1448dae8c6d21016ee3adc0
Vlad Kachmazov <kachevl@gmail.com>
2025-01-13 16:34:16 +0300

    solution

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,7 +8,7 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof validateEmail('luckyGuy@gmail.com')).toBe('boolean');
   });
 
   it(`should return 'true' for the valid email`, () => {
@@ -16,5 +16,31 @@ describe(`Function 'validateEmail':`, () => {
       .toBeTruthy();
   });
 
-  // write more tests here
+  it('returns "false" for email with not latin alphabet', () => {
+    expect(validateEmail('тест@alpha.com')).toBeFalsy();
+  });
+
+  it('returns "false" for email without @', () => {
+    expect(validateEmail('hamsteralpha.com')).toBeFalsy();
+  });
+
+  it('returns "false" for email starting with dot', () => {
+    expect(validateEmail('.bigguy@gmail.com')).toBeFalsy();
+  });
+
+  it('returns "false" for email with domain starting with dot', () => {
+    expect(validateEmail('bigboss@.gmail.com')).toBeFalsy();
+  });
+
+  it("returns 'false' for email with ! $ % & ' * + / = ? ^ { | } ~", () => {
+    const symbols = "!$%&'*+/=?^{|}~";
+
+    for (const s of symbols) {
+      expect(validateEmail(`bad${s}email@gmail.com`)).toBeFalsy();
+    }
+  });
+
+  it('returns "false" for email with double dots in personal part', () => {
+    expect(validateEmail('big:boss@gmail.com')).toBeFalsy();
+  });
 });
